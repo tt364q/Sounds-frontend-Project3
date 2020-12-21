42592d44c40945b9a64079fbf3c64ce9f9dca335
Taylor Torres <taylor23torres@gmail.com>
2020-12-20 21:02:57 -0600

    updates backend url

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
 import { setToken, getUserFromToken, removeToken } from './tokenService';
 
-const BASE_URL = 'http://localhost:3001/api';
+const BASE_URL = 'https://git.heroku.com/project3-sounds-backend.git';
 
 
 
